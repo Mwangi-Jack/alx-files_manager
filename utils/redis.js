7571a728edb29f5b41ad9390e0b5f8f1d5eddfde
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-09-06 13:48:15 +0300

    setting up redis utils

---
+++ utils/redis.js
@@ -0,0 +1,38 @@
+const redis = require('redis');
+
+
+const client = redis.createClient({
+	url: 'redis://localhost:6379'
+});
+
+class RedisClient{
+	constructor(){
+		client.connect()
+		client.on('err', (err) => {
+			console.log(err)
+		})
+	}
+
+	isAlive() {
+		return client ? true : false;
+	}
+
+	async get(key) {
+		const value =  await client.get(key);
+		return value;
+	}
+
+	async set(key, val, dur) {
+		await client.set(key, val, {
+			EX: dur
+		});
+	}
+
+	async del(key) {
+		await cleint.del(key);
+	}
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
